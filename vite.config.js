141beba352e5a4e00f779b1e87bf3f7edb721d9b
CodeKouser <kouserbanu3@gmail.com>
2025-09-21 08:44:00 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+
+	// This is the fix: tell Vite/Rollup to not bundle these Node.js modules
+	// and treat them as external dependencies available in the runtime.
+	ssr: {
+		noExternal: ['mongodb']
+	}
 });
